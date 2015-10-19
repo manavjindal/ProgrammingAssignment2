6419f4ba80fd902e73176ed0d8c41b1a1a7a2b61
Manav Jindal <er.manav.jindal@gmail.com>
2015-10-20 00:29:10 +0530

    Programming Assignment 2 - Create cachematrix function

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        v_inv <- NULL
+        
+        set <- function(z) {
+                x <<- z
+                v_inv <<- NULL
+        }
+        
+        get <- function() x
+        
+        setinv <- function(inverse)  v_inv <<- inverse
+        
+        getinv <- function() v_inv
+        
+        list(set=set, 
+             get=get, 
+             setinv=setinv, 
+             getinv=getinv)
 
 }
 
@@ -12,4 +30,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        v_inv <- x$getinv()
+        
+        if(!is.null(v_inv)) {
+                message("Getting cached data, please wait")
+                return(v_inv)
+        }
+        
+        data <- x$get()
+        
+        v_inv <- solve(data, ...)
+        
+        x$setinv(v_inv)
+        
+        return(v_inv)
 }
